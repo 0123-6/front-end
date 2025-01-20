8a0316da4e25f08ab55fb1833adebb9ab69ff796
江江909458209 <909458209@qq.com>
2025-01-20 23:42:42 +0800

    代码优化

--- template-project/Vue3/src/util/validator.ts
+++ template-project/Vue3/src/util/validator.ts
@@ -12,42 +12,42 @@ export const isPasswordRegExp = /^\w{8,16}$/
 
 // 表单校验规则
 export const formRules = {
-    phone: (_this) => [
-        {required: true, trigger: 'change', message: '请输入手机号码',},
-        {pattern: isPhoneRegExp, trigger: 'change', message: '手机号码格式不正确',},
-    ],
-    verificationCode: (_this) => [
-        {required: true, trigger: 'change', message: '请输入验证码',},
-        {pattern: isVerificationCodeRegExp, trigger: 'change', message: '验证码格式不正确',},
-    ],
-    password: (_this) => [
-        {required: true, trigger: 'change', message: '请输入密码',},
-        {pattern: isPasswordRegExp, trigger: 'change', message: '只能包含数字，字母，下划线，8-16位'},
-        {
-            validator: (_rule, _value, callback) => {
-                // @ts-ignore
-                console.log(_rule)
-                console.log(_value)
-                console.log('_this: ', _this)
-                _this.$refs.formRef!.validateField('password2')
-                callback()
-            },
-            trigger: 'change',
-        }
-    ],
-    password2: (_this) => [
-        {required: true, trigger: 'change', message: '请输入确认密码',},
-        {pattern: isPasswordRegExp, trigger: 'change', message: '只能包含数字，字母，下划线，8-16位'},
-        {
-            validator: (_rule, _value, callback) => {
-                // @ts-ignore
-                if (_this.formModel.password2 !== _this.formModel.password) {
-                    callback(new Error('两次密码不一致'))
-                } else {
-                    callback()
-                }
-            },
-            trigger: 'change',
-        },
-    ],
+	phone: (_this) => [
+		{required: true, trigger: 'change', message: '请输入手机号码',},
+		{pattern: isPhoneRegExp, trigger: 'change', message: '手机号码格式不正确',},
+	],
+	verificationCode: (_this) => [
+		{required: true, trigger: 'change', message: '请输入验证码',},
+		{pattern: isVerificationCodeRegExp, trigger: 'change', message: '验证码格式不正确',},
+	],
+	password: (_this) => [
+		{required: true, trigger: 'change', message: '请输入密码',},
+		{pattern: isPasswordRegExp, trigger: 'change', message: '只能包含数字，字母，下划线，8-16位'},
+		{
+			validator: (_rule, _value, callback) => {
+				// @ts-ignore
+				console.log(_rule)
+				console.log(_value)
+				console.log('_this: ', _this)
+				_this.$refs.formRef!.validateField('password2')
+				callback()
+			},
+			trigger: 'change',
+		}
+	],
+	password2: (_this) => [
+		{required: true, trigger: 'change', message: '请输入确认密码',},
+		{pattern: isPasswordRegExp, trigger: 'change', message: '只能包含数字，字母，下划线，8-16位'},
+		{
+			validator: (_rule, _value, callback) => {
+				// @ts-ignore
+				if (_this.formModel.password2 !== _this.formModel.password) {
+					callback(new Error('两次密码不一致'))
+				} else {
+					callback()
+				}
+			},
+			trigger: 'change',
+		},
+	],
 }
